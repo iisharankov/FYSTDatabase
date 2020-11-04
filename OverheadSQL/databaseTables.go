eecba80166383e0e68b7846b02d7336b40523b21
iisharankov <ivansharankov3@gmail.com>
2020-11-04 11:06:01 -0500

    adds all Database tables as structs

--- OverheadSQL/databaseTables.go
+++ OverheadSQL/databaseTables.go
@@ -1,20 +1,44 @@
 package overheadsql
 
-type backupLocation struct {
+type backupLocationTable struct {
 	LocationID      int
 	LocationName    string
 	LocationAddress string
 	FTPIPAddress    string
 }
 
-// InstrumentTable is a SQL table changed to a struct
-// type InstrumentTable struct {
-// 	InstrumentID   int
-// 	InstrumentName string
-// 	FullName       string
-// 	Description    string
-// 	NumberOfPixels int
-// 	FrequencyMin   int
-// 	FrequencyMax   int
-// 	TempRange      int
-// }
+type instrumentTable struct {
+	InstrumentID   int
+	InstrumentName string
+	FullName       string
+	Description    string
+	NumberOfPixels int
+	FrequencyMin   int
+	FrequencyMax   int
+	TempRange      int
+}
+
+type objectFileTable struct {
+	FileID         int
+	DateCreated    string // dataetime?
+	InstrumentID   int
+	Size           int
+	HashOfBytes    string
+	LocationOnDisk string
+	ObjectStorage  string
+}
+
+type ruleTable struct {
+	RuleID          int
+	RuleDescription int
+	InstrumentID    int
+	LocationIDint   int
+	Active          int // tinyint or bool
+}
+
+type logTable struct {
+	FileID     int
+	RuleID     int
+	BackupDate string // datetime?
+	IsCopying  int    // tiny int or bool
+}
