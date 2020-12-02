ae848ff4db3c0d098588e3f52bd8cb0cfc00e389
iisharankov <ivansharankov3@gmail.com>
2020-12-02 17:22:07 -0500

    adds json names to structures

--- OverheadSQL/databaseTables.go
+++ OverheadSQL/databaseTables.go
@@ -1,44 +1,44 @@
 package overheadsql
 
 type BackupLocationTable struct {
-	LocationID      int
-	LocationName    string
-	LocationAddress string
-	FTPIPAddress    string
+	LocationID      int    `json:"location_id"`
+	LocationName    string `json:"location_name"`
+	LocationAddress string `json:"location_address"`
+	FTPIPAddress    string `json:"ftp_ip_address"`
 }
 
 type InstrumentTable struct {
-	InstrumentID   int
-	InstrumentName string
-	FullName       string
-	Description    string
-	NumberOfPixels int
-	FrequencyMin   int
-	FrequencyMax   int
-	TempRange      int
+	InstrumentID   int    `json:"instrument_id"`
+	InstrumentName string `json:"instrument_name"`
+	FullName       string `json:"full_name"`
+	Description    string `json:"description"`
+	NumberOfPixels int    `json:"number_of_pixels"`
+	FrequencyMin   int    `json:"frequency_min"`
+	FrequencyMax   int    `json:"frequency_max"`
+	TempRange      int    `json:"temp_range"`
 }
 
 type ObjectFileTable struct {
-	FileID         int
-	DateCreated    string // dataetime?
-	InstrumentID   int
-	Size           int
-	HashOfBytes    string
-	LocationOnDisk string
-	ObjectStorage  string
+	FileID         int    `json:"file_id"`
+	DateCreated    string `json:"date_created"` // dataetime?
+	InstrumentID   int    `json:"instrument_id"`
+	Size           int    `json:"size"`
+	HashOfBytes    string `json:"hash_of_bytes"`
+	LocationOnDisk string `json:"location_on_disk"`
+	ObjectStorage  string `json:"object_storage"`
 }
 
 type RuleTable struct {
-	RuleID          int
-	RuleDescription string
-	InstrumentID    int
-	LocationID      int
-	Active          int // tinyint or bool
+	RuleID          int    `json:"rule_id"`
+	RuleDescription string `json:"rule_description"`
+	InstrumentID    int    `json:"instrument_id"`
+	LocationID      int    `json:"location_id"`
+	Active          int    `json:"active"` // tinyint or bool
 }
 
 type LogTable struct {
-	FileID     int
-	RuleID     int
-	BackupDate string // datetime?
-	IsCopying  int    // tiny int or bool
+	FileID     int    `json:"file_id"`
+	RuleID     int    `json:"rule_id"`
+	BackupDate string `json:"backup_date"` // datetime?
+	IsCopying  int    `json:"is_copying"`  // tiny int or bool
 }
