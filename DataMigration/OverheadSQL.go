6fef23337ebb07dc7b4ed93a8e8aa431c85b4d40
iisharankov <ivansharankov3@gmail.com>
2021-03-06 22:19:01 -0500

    Rewrites methods with proper returns and adds reconnect method in CheckConnection

--- DataMigration/OverheadSQL.go
+++ DataMigration/OverheadSQL.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"database/sql"
-	"errors"
 	"fmt"
 	"log"
 	"reflect"
@@ -21,10 +20,12 @@ type DatabaseConnection struct {
 // Connect connects to a database
 func (dpCon *DatabaseConnection) Connect(dbUsername, dbPassword, dbIP, dpName string) error {
 	SQLConnectionString := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", dbUsername, dbPassword, dbIP, dpName)
+
 	db, err := sql.Open("mysql", SQLConnectionString)
 	if err != nil {
 		return err
 	}
+
 	if err := db.Ping(); err != nil {
 		return err
 	}
@@ -47,56 +48,77 @@ func (dpCon *DatabaseConnection) Connect(dbUsername, dbPassword, dbIP, dpName st
 // 	return dbCon.DBConnection, nil
 // }
 
-// CheckConnection checks if the connection to the database is active
-func (dpCon *DatabaseConnection) CheckConnection() error {
+// CheckConnection checks if the connection to the database is active, reconnect if necessary
+func (dpCon *DatabaseConnection) CheckConnection() (err error) {
+
 	// Connection may be nil if never established
 	if dbCon.DBConnection == nil {
-		errMsg := errors.New("No connection to database established, could not connect")
-		return errMsg
+		log.Println("No connection to database established, attempting to connect")
+		if dbCon.Connect(dbUsername, dbPassword, dbAddress, dbName); err != nil {
+			log.Println("Database reconnect failed")
+			return err
+		}
 	}
-	err := dbCon.DBConnection.Ping()
-	if err != nil {
-		return err
+
+	if dbCon.DBConnection.Ping(); err != nil {
+		log.Println("Ping to Database returned error, reconnecting")
+		dbCon.DBConnection = nil
+
+		if dbCon.Connect(dbUsername, dbPassword, dbAddress, dbName); err != nil {
+			log.Println("Database reconnect failed")
+			return err
+		}
 	}
 	return nil
 }
 
 // PrepareQuery prepares a query with the given string
-func (dpCon *DatabaseConnection) PrepareQuery(prepareString string) (*sql.Stmt, error) {
+func (dpCon *DatabaseConnection) PrepareQuery(prepareString string) (stmt *sql.Stmt, err error) {
+	if dbCon.CheckConnection(); err != nil {
+		return nil, err
+	}
 
-	stmt, err := dbCon.DBConnection.Prepare(prepareString)
+	stmt, err = dbCon.DBConnection.Prepare(prepareString)
 	return stmt, err
 }
 
 // QueryWriteWithTransaction takes a query and executes it with a transaction for safety
-// TODO: make input a list of queries instead of just one!
-func (dpCon *DatabaseConnection) QueryWriteWithTransaction(insertStatement []string) {
+func (dpCon *DatabaseConnection) QueryWriteWithTransaction(insertStatement []string) (err error) {
+	if dbCon.CheckConnection(); err != nil {
+		return err
+	}
 
 	ctx := context.Background() // Create a new context, and begin a transaction
 	tx, err := dbCon.DBConnection.BeginTx(ctx, nil)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
+
 	// tx is instance of *sql.Tx where queries can be executed
 	for _, val := range insertStatement {
 		_, err = tx.ExecContext(ctx, val)
 		if err != nil {
 			tx.Rollback() // rollback transaction if error returned
-			return
+			return err
 		}
 	}
 
 	// tx.Commit() will fail if tx.Rollback() is called in above loop,
 	// so this is safe to leave outside the loop.
-	err = tx.Commit()
-	if err != nil {
-		log.Fatal(err)
+	if err = tx.Commit(); err != nil {
+		return err
 	}
+
+	return nil
 }
 
 // QueryWrite takes a single query and executes it with no transactional safety
-func (dpCon *DatabaseConnection) QueryWrite(insertStatement string) error {
-	_, err := dbCon.DBConnection.Query(insertStatement)
+func (dpCon *DatabaseConnection) QueryWrite(insertStatement string) (err error) {
+	if dbCon.CheckConnection(); err != nil {
+		return err
+	}
+
+	_, err = dbCon.DBConnection.Query(insertStatement)
 	if err != nil {
 		return err
 	}
@@ -105,8 +127,7 @@ func (dpCon *DatabaseConnection) QueryWrite(insertStatement string) error {
 
 // QueryRead takes a query returns a list of all the rows returned by the database
 func (dpCon *DatabaseConnection) QueryRead(SQLQuery string, p interface{}) (emptyOutput reflect.Value, err error) {
-	err = dbCon.CheckConnection()
-	if err != nil {
+	if dbCon.CheckConnection(); err != nil {
 		return emptyOutput, err
 	}
 
