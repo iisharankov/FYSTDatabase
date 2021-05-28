1f63e14f8ababfea3a744c8c4a9cd266cac0debb
iisharankov <ivansharankov3@gmail.com>
2021-05-28 01:34:56 -0400

    cleans up router endpoints names and jsonResponse

--- DataMigration/API.go
+++ DataMigration/API.go
@@ -5,65 +5,56 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
 
 	"github.com/gorilla/mux"
+	"github.com/iisharankov/FYSTDatabase/datasets"
 )
 
 func concatErrors(err error, newError string) error {
-	var errstrings []string
-	errstrings = append(errstrings, err.Error())
-
-	// Concatonate error with additional one.
-	errstrings = append(errstrings, fmt.Errorf(newError).Error())
-
-	// combine and return both errors for more useful debugging for user
-	combinedErrors := fmt.Errorf(strings.Join(errstrings, " - "))
-	return combinedErrors
+	return fmt.Errorf(newError + " - " + err.Error())
 }
 
 // Shamelessly stolen
 func jsonResponse(w http.ResponseWriter, err error, statusCode int) {
-	var response struct {
-		S string `json:"status"`
-		M string `json:"message,omitempty"`
-	}
+	var response datasets.ServerHTTPResponse
 
-	if err != nil {
-		log.Println(err) // Catch all for errors for logging
+	// v // Encode message M and Status S depending on statusCode
+	if 200 <= statusCode && statusCode < 300 { //
+		response.S = "ok"
+	} else {
 		response.S = "error"
 		response.M = err.Error()
-	} else {
-		response.S = "ok"
-		statusCode = http.StatusOK
 	}
+	// ^ //
 
-	// TODO: Write vs writeHeader?
+	// v // Write header then populate body
 	w.WriteHeader(statusCode)
 	err = json.NewEncoder(w).Encode(response)
 	if err != nil {
-		log.Print(err)
+		log.Println("Error while encoding jsonResponse body: ", err)
 	}
+	// ^ //
 }
 
 func startAPIServer() {
 
 	// Containers fail if db connection is first
 	if err := dbCon.Connect(dbUsername, dbPassword, dbAddress, dbName); err != nil {
-		log.Println(err)
+		log.Println(err) // TODO: Panic if database cannot connect
 	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/files", AddFileToDatabaseEndpoint).Methods("POST")
-	router.HandleFunc("/files", GetAllFilesFromDBEndpoint).Methods("GET")
-	router.HandleFunc("/files/{id}", GetFilesFromDBEndpoint).Methods("GET")
+	router.HandleFunc("/files", GetFilesFromDBEndpoint).Methods("GET")
+	router.HandleFunc("/files/{filename}", GetFileFromDBEndpoint).Methods("GET")
+	router.HandleFunc("/files/{filename}/copies", AddRowToCopiesTableEndpoint).Methods("POST")
 
-	router.HandleFunc("/logs/{id}/{location}", AddLogToDBEndpoint).Methods("POST")
-	router.HandleFunc("/logs", GetAllLogsFromDBEndpoint).Methods("GET")
-	router.HandleFunc("/logs/{id}", GetLogsFromDBEndpoint).Methods("GET")
-	router.HandleFunc("/logs/{id}/copies", GetCopiesOFLogFromDBEndpoint).Methods("GET")
+	router.HandleFunc("/records/{filename}/{location}", AddRecordToDBEndpoint).Methods("POST")
+	router.HandleFunc("/records", GetAllRecordsFromDBEndpoint).Methods("GET")
+	router.HandleFunc("/records/{filename}", GetRecordFromDBEndpoint).Methods("GET")
 
 	router.HandleFunc("/rules", GetAllRulesFromDBEndpoint).Methods("GET")
 	router.HandleFunc("/rules/{id}", GetRulesFromDBEndpoint).Methods("GET")
+
 	log.Fatal(http.ListenAndServe(":8700", router))
 }
